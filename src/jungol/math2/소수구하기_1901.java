b2639f36ade65b8f1c9113f490535e4984933907
chaeyeonkim0223 <tomybaru@naver.com>
2021-06-29 11:40:56 +0900

    0629

---
+++ src/jungol/math2/소수구하기_1901.java
@@ -0,0 +1,45 @@
+package jungol.math2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class 소수구하기_1901 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			int num = sc.nextInt();
+			if(isPrime(num)) {
+				System.out.println(num);
+			}
+			else {
+				//내 앞,뒤 소수만 확인해봐도 됨. 굳이 2~1000000개의 소수를 다 구하지 않아도 된다.
+				boolean flag = false;
+				for (int j = 0; !flag; j++) {
+					if(isPrime(num-j)) {
+						System.out.print(num-j+" ");
+						flag = true;
+					}
+					if(isPrime(num+j)) {
+						System.out.print(num+j);
+						flag = true;
+					}
+				}System.out.println();
+			}
+		}
+		
+	}static boolean isPrime(int number) {
+		if(number < 2) return false;
+		int sq = (int)Math.sqrt(number);
+		//1과 자신 이외에 약수가 있으면 소수가 아니다. 
+		//수가 커질 경우를 대비해 제곱근으로 계산
+		for (int i = 2; i <= sq; i++) {
+			if(number%i==0) return false;
+		}
+		return true;
+	}
+
+}
