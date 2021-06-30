ec1886a3ddb29bb48a3387111e858d33b109561d
chaeyeonkim0223 <tomybaru@naver.com>
2021-07-01 03:26:11 +0900

    eclipse push test

--- src/jungol/string/Main.java
+++ src/jungol/string/Main.java
@@ -3,7 +3,7 @@ package jungol.string;
 import java.util.Scanner;
 
 public class Main {
-
+//
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		char [] str = sc.next().toCharArray();
