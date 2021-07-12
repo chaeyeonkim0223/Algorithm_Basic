a9b56aeb1f18711e4e3b756beb206003669db35e
김채연 <tomybaru@naver.com>
2021-07-13 03:19:48 +0900

    0713

---
+++ src/jungol/data_processing/큐_1697.java
@@ -0,0 +1,30 @@
+package jungol.data_processing;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class 큐_1697 {
+
+	public static void main(String[] args) {
+		Scanner  sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Queue<Integer> q = new LinkedList<>();	
+		for (int i = 0; i < n ; i++) {
+			char now = sc.next().charAt(0);
+			if(now =='i') {
+				q.add(sc.nextInt());
+			}else if(now=='o'){
+				if(q.isEmpty()) {
+					System.out.println("empty");
+				}else {
+					System.out.println(q.poll());
+				}
+			}else {
+				System.out.println(q.size());
+			}
+		}
+	}
+
+}
