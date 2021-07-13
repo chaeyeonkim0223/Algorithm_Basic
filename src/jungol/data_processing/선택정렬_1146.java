c1ce8e44c66aeb42c62cab0d9032343bcb3f45e9
김채연 <tomybaru@naver.com>
2021-07-14 03:25:14 +0900

    0714

---
+++ src/jungol/data_processing/선택정렬_1146.java
@@ -0,0 +1,38 @@
+package jungol.data_processing;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 선택정렬_1146 {
+	
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int [n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int start=0;
+		int size = arr.length-1;
+		while(size-->0) {
+			//최솟값 찾기
+			int min=Integer.MAX_VALUE;
+			for (int i = start; i < arr.length; i++) {
+				if(arr[i]<min) min = arr[i];
+			}
+			//맨앞과 바꾸기
+			for (int i = start; i < arr.length; i++) {
+				if(min == arr[i]) {
+					int temp = arr[i];
+					arr[i] = arr[start];
+					arr[start] = temp;
+					start+=1;
+					break;
+				}
+			}
+			for (int i = 0; i < arr.length; i++) {
+				System.out.print(arr[i]+" ");
+			}System.out.println();
+		}
+	}
+}
