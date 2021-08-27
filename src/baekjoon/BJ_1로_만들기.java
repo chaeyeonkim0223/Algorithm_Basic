875113df7045430004a8251221646487cf3305fc
김채연 <86418158+chaeyeonkim0223@users.noreply.github.com>
2021-08-27 13:22:24 +0900

    DP 연습

---
+++ src/baekjoon/BJ_1로_만들기.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BJ_1로_만들기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int[]dp = new int [x+1];
+		for(int i=2; i<=x; i++) {
+			dp[i] = dp[i-1]+1;
+			if(i%2==0) dp[i]= Math.min(dp[i], dp[i/2]+1);
+			if(i%3==0) dp[i]= Math.min(dp[i], dp[i/3]+1);
+		}
+//		System.out.println(Arrays.toString(dp));
+		System.out.println(dp[x]);
+	}
+
+}
