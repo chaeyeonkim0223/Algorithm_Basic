c26c7d9c3f0c80436b73d3e71aaa8330b1b36973
김채연 <86418158+chaeyeonkim0223@users.noreply.github.com>
2021-08-27 22:52:59 +0900

    2019 KAKAO BLIND RECRUITMENT

--- src/programmers/실패율.java
+++ src/programmers/실패율.java
@@ -1,78 +1,61 @@
 package programmers;
-	
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-	
-	public class 실패율 {
-		static int N = 4;
-		static int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
-//		static int[] stages = {4,4,4,4,4};
-		public static void main(String[] args) {
-			int answer[] = new int[N];
-			answer = solution(N,stages);
-		}
-		private static int[] solution(int N, int[] stages) {
-			// 실패율, state번호를 객체배열에 저장
-			List<Point> list = new ArrayList<>();
-			Point[] pp = new Point[N+1];
-//			pp[0] = new Point(0, 0);
-			for (int i = 1; i <= N; i++) {
-				int fail = 0;//실패한사람
-				int success =0;//성공한사람
-				for (int j = 0; j < stages.length; j++) {
-					if(i>=stages[j] && stages[j]>i-1) {
-						//도달은 했으나 클리어하지 못한 플레이어의 수
-						fail++;
-					}	
-					if(i<=stages[j]) {
-						//도달한 플레이어의 수  
-						success++;
-					}
-				}
-				if(success==0) {
-					Point p = new Point(i, 0);
-					list.add(p);
-//					pp[i] = p;
-				}else {
-					Point p = new Point(i, (double)fail/success);
-					list.add(p);
-//					pp[i] = p;
-				}
-				
-			}
-			Collections.sort(list);
-//			Arrays.sort(pp);
-			int[] answer = new int [N];
-			System.out.println(Arrays.toString(pp));
-			for (int i = 0; i < list.size(); i++) {
-				answer[i]= list.get(i).stateNum;
-			}
-			return answer;
-		}
-		static class Point implements Comparable<Point>{
-			int stateNum=0;
-			double failRate=0;
-			public Point(int stateNum, double failRate) {
-				super();
-				this.stateNum = stateNum;
-				this.failRate = failRate;
-			}
-			@Override
-			public String toString() {
-				return "Point [stateNum=" + stateNum + ", failRate=" + failRate + "]";
-			}
 
-			@Override
-			public int compareTo(Point o) {
-				if(Double.compare(o.failRate, failRate) == 0) {				
-					return stateNum - o.stateNum;
-				}
-				else {
-					return Double.compare(o.failRate, failRate);
-				}
-			}
-		}
-	
+import java.util.Arrays;
+
+public class 실패율 {
+
+	public static void main(String[] args) {
+
 	}
+	static class Solution {
+	    public int[] solution(int N, int[] stages) {
+	        Node[] arr = new Node[N];
+	        for(int i=1; i<=N; i++){
+	            int fail=0;
+	            int success=0;
+	            double failRate=0.0;
+	            for(int j=0; j<stages.length; j++){
+	                if(stages[j]==i) fail++;
+	                if(stages[j]>=i) success++;
+	            }
+	            System.out.println(fail +" " + success);
+	            if(fail == 0){
+	                failRate=0;
+	            }else{
+	                failRate = (double)fail/success;    
+	            }
+	            arr[i-1] = new Node(i, failRate);
+	        }
+	        Arrays.sort(arr);
+	        int[] answer = new int [N];
+	        int idx=0;
+	        for(Node now: arr){
+	            answer[idx++] = now.num;
+	        }
+	        return answer;
+	    }
+	    static class Node implements Comparable<Node>{
+	        int num;
+	        double failRate;
+	        public Node(int num, double failRate){
+	            this.num = num;
+	            this.failRate = failRate;
+	        }
+	        @Override
+	        public int compareTo(Node o){
+	            if(Double.compare(o.failRate,failRate)==0){
+	                // 번호 오름차순
+	                return this.num - o.num;
+	            }else{
+	                // 실패율 내림차순
+	                return Double.compare(o.failRate,failRate);
+	            }
+	        }
+	        @Override
+	        public String toString(){
+	            return "num  "+ this.num + "  failRate  " + this.failRate;
+	        }
+	    }
+	}
+
+}
